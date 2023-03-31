1d1a77d831c60d65dde14ac76ed49b93228299d3
trmcnvn <hello@trmcnvn.dev>
2023-03-31 20:12:57 +1100

    update to latest

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,12 @@
-import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
   },
 
-  preprocess: [
-    preprocess({
-      postcss: true,
-    }),
-  ],
+  preprocess: [vitePreprocess()],
 };
 
 export default config;
