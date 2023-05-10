d586122a7fc6939f24b6534780bf2cc1505ad208
maceq687 <maciek@softman.pl>
2023-05-10 13:55:22 +0200

    Updates

--- _app/immutable/chunks/2-0bd67aa4.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-c73e0039.js";export{t as component};
---
+++ _app/immutable/chunks/2-4e13e112.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-1810d1f8.js";export{t as component};
